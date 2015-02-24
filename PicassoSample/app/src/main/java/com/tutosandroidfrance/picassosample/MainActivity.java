6080346414b4736180b4e23148b0365ee08c6f26
Florent <Florent>
2015-02-25 00:09:39 +0100

    Import de PicassoSample

---
+++ PicassoSample/app/src/main/java/com/tutosandroidfrance/picassosample/MainActivity.java
@@ -0,0 +1,25 @@
+package com.tutosandroidfrance.picassosample;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(getBaseContext()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+    }
+}
