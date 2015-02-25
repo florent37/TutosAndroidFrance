679842dc4a948583cf453f8a00d871edf1f79fb0
Florent <champigny.florent@gmail.com>
2015-02-26 00:52:18 +0100

    added blur transformation

--- PicassoSample/app/src/main/java/com/tutosandroidfrance/picassosample/MainActivity.java
+++ PicassoSample/app/src/main/java/com/tutosandroidfrance/picassosample/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tutosandroidfrance.picassosample;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,6 +9,7 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -20,6 +23,35 @@ public class MainActivity extends ActionBarActivity {
 
         imageView = (ImageView) findViewById(R.id.imageView);
 
-        Picasso.with(getBaseContext()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+        Picasso.with(getBaseContext()).load("http://i.imgur.com/DvpvklR.png")
+                .transform(new BlurTransformation(this))
+                .into(imageView);
+    }
+
+    public class BlurTransformation implements Transformation {
+        private Context context;
+
+        public BlurTransformation(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public Bitmap transform(Bitmap source) {
+            //Source from : https://raw.githubusercontent.com/PomepuyN/BlurEffectForAndroidDesign/master/BlurEffect/src/com/npi/blureffect/Blur.java
+            //Copyright : PomepuyN
+            Bitmap result = Blur.fastblur(context,source,10);
+
+            //si l'image a été modifiée, on désalloue la mémoire occupée par l'ancienne Bitmap
+            if (result != source) {
+                source.recycle();
+            }
+            return result;
+        }
+
+        //la clé va être utilisée lors de la sauvegarde en cache
+        @Override
+        public String key() {
+            return "BlurTransformation";
+        }
     }
 }
