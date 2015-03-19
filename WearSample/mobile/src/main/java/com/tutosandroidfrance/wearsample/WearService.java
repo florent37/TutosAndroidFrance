97b8c724bfca51e3295dc4371fe5e7280e2744d4
Florent <champigny.florent@gmail.com>
2015-03-19 01:36:33 +0100

    Added Wear Sample

---
+++ WearSample/mobile/src/main/java/com/tutosandroidfrance/wearsample/WearService.java
@@ -0,0 +1,202 @@
+package com.tutosandroidfrance.wearsample;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+import java.io.ByteArrayOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class WearService extends WearableListenerService {
+
+    private final static String TAG = WearService.class.getCanonicalName();
+
+    protected GoogleApiClient mApiClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+        mApiClient.connect();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mApiClient.disconnect();
+    }
+
+    /**
+     * Appellé à la réception d'un message envoyé depuis la montre
+     *
+     * @param messageEvent message reçu
+     */
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        super.onMessageReceived(messageEvent);
+
+        //Ouvre une connexion vers la montre
+        ConnectionResult connectionResult = mApiClient.blockingConnect(30, TimeUnit.SECONDS);
+
+        if (!connectionResult.isSuccess()) {
+            Log.e(TAG, "Failed to connect to GoogleApiClient.");
+            return;
+        }
+
+        //traite le message reçu
+        final String path = messageEvent.getPath();
+
+        if (path.equals("bonjour")) {
+
+            AndroidService androidService = new RestAdapter.Builder()
+                    .setEndpoint(AndroidService.ENDPOINT)
+                    .build().create(AndroidService.class);
+
+            androidService.getElements(new Callback<List<Element>>() {
+                @Override
+                public void success(List<Element> elements, Response response) {
+                    envoyerListElements(elements);
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+                }
+            });
+
+        }
+    }
+
+    private void envoyerListElements(final List<Element> elements) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Envoie des elements
+                sendElements(elements);
+
+                //puis indique à la montre le nombre d'éléments à afficher
+                sendMessage("nombre_elements",String.valueOf(elements.size()));
+            }
+        }).start();
+    }
+
+    /**
+     * Envoie un message à la montre
+     *
+     * @param path    identifiant du message
+     * @param message message à transmettre
+     */
+    protected void sendMessage(final String path, final String message) {
+        //effectué dans un trhead afin de ne pas être bloquant
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //envoie le message à tous les noeuds/montres connectées
+                final NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi.getConnectedNodes(mApiClient).await();
+                for (Node node : nodes.getNodes()) {
+                    Wearable.MessageApi.sendMessage(mApiClient, node.getId(), path, message.getBytes()).await();
+
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * Récupère une bitmap à partir d'une URL
+     */
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(src).openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            return BitmapFactory.decodeStream(connection.getInputStream());
+        } catch (Exception e) {
+            // Log exception
+            return null;
+        }
+    }
+
+    /**
+     * Les bitmap transferés depuis les DataApi doivent être empaquetées en Asset
+     */
+    public static Asset createAssetFromBitmap(Bitmap bitmap) {
+        final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteStream);
+        return Asset.createFromBytes(byteStream.toByteArray());
+    }
+
+    /**
+     * Permet d'envoyer une liste d'elements
+     */
+    protected void sendElements(final List<Element> elements) {
+
+        for (int position = 0; position < elements.size(); ++position) {
+
+            Element element = elements.get(position);
+
+            //créé un emplacement mémoire "element/[position]"
+            final PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/element/" + position);
+
+            //ajoute la date de mise à jour
+            putDataMapRequest.getDataMap().putString("timestamp", new Date().toString());
+
+            //ajoute l'element champ par champ
+            putDataMapRequest.getDataMap().putString("titre", element.getTitre());
+            putDataMapRequest.getDataMap().putString("description", element.getDescription());
+            putDataMapRequest.getDataMap().putString("url", element.getUrl());
+
+            //envoie la donnée à la montre
+            if (mApiClient.isConnected())
+                Wearable.DataApi.putDataItem(mApiClient, putDataMapRequest.asPutDataRequest());
+
+            //puis envoie l'image associée
+            sendImage(element.getUrl(), position);
+        }
+    }
+
+    /**
+     * Permet d'envoyer une image à la montre
+     */
+    protected void sendImage(String url, int position) {
+        //télécharge l'image
+        Bitmap bitmap = getBitmapFromURL(url);
+        if (bitmap != null) {
+            Asset asset = createAssetFromBitmap(bitmap);
+
+            //créé un emplacement mémoire "image/[url_image]"
+            final PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/image/" + position);
+
+            //ajoute la date de mise à jour
+            putDataMapRequest.getDataMap().putString("timestamp", new Date().toString());
+
+            //ajoute l'image
+            putDataMapRequest.getDataMap().putAsset("image", asset);
+
+            //envoie la donnée à la montre
+            if (mApiClient.isConnected())
+                Wearable.DataApi.putDataItem(mApiClient, putDataMapRequest.asPutDataRequest());
+        }
+    }
+
+}
---
+++ WearSample/wear/src/main/java/com/tutosandroidfrance/wearsample/ElementGridPagerAdapter.java
@@ -0,0 +1,95 @@
+package com.tutosandroidfrance.wearsample;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.wearable.view.CardFragment;
+import android.support.wearable.view.FragmentGridPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElementGridPagerAdapter extends FragmentGridPagerAdapter {
+
+    private Context context;
+    private List<Row> mRows = new ArrayList<Row>();
+    private List<Element> mElements;
+
+    public ElementGridPagerAdapter(Context context, List<Element> elements, FragmentManager fm) {
+        super(fm);
+        this.context = context;
+        this.mElements = elements;
+
+        //Construit le tableau des éléménts à afficher
+        for (Element element : elements) {
+            mRows.add(new Row(
+                            //pour l'instant nous ne mettrons qu'un élément par ligne
+                            CardFragment.create(element.getTitre(), element.getTexte())
+                    )
+            );
+        }
+    }
+
+    //Le fragment à afficher p
+    @Override
+    public Fragment getFragment(int row, int col) {
+        Row adapterRow = mRows.get(row);
+        return adapterRow.getColumn(col);
+    }
+
+    //le drawable affichée en background pour la ligne [row]
+    @Override
+    public Drawable getBackgroundForRow(final int row) {
+        Bitmap bitmap = ((MainActivity) context).getBitmap(row);
+        if (bitmap != null)
+            return new BitmapDrawable(context.getResources(), bitmap);
+        else
+            return BACKGROUND_NONE;
+    }
+
+    @Override
+    public Drawable getBackgroundForPage(final int row, final int column) {
+        //nous pouvons spécifier un drawable différent pour le swipe horizontal
+        return getBackgroundForRow(row);
+    }
+
+    //Le nombre de lignes dans la grille
+    @Override
+    public int getRowCount() {
+        return mRows.size();
+    }
+
+    //Le nombre de colonnes par ligne
+    @Override
+    public int getColumnCount(int rowNum) {
+        return mRows.get(rowNum).getColumnCount();
+    }
+
+    /**
+     * Représentation d'une ligne - Contient une liste de fragments
+     */
+    private class Row {
+        final List<Fragment> columns = new ArrayList<Fragment>();
+
+        public Row(Fragment... fragments) {
+            for (Fragment f : fragments) {
+                add(f);
+            }
+        }
+
+        public void add(Fragment f) {
+            columns.add(f);
+        }
+
+        Fragment getColumn(int i) {
+            return columns.get(i);
+        }
+
+        public int getColumnCount() {
+            return columns.size();
+        }
+    }
+}
