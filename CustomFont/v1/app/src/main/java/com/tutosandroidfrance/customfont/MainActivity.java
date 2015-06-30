aafe345d78701ebe59a486da998f04753f4ccd0e
Florent <champigny.florent@gmail.com>
2015-06-30 17:17:09 +0200

    added custom view

---
+++ CustomFont/v1/app/src/main/java/com/tutosandroidfrance/customfont/MainActivity.java
@@ -0,0 +1,36 @@
+package com.tutosandroidfrance.customfont;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView1 = (TextView) findViewById(R.id.text1);
+        TextView textView2 = (TextView) findViewById(R.id.text2);
+        TextView textView3 = (TextView) findViewById(R.id.text3);
+
+        setFont(textView2,"Roboto-Light.ttf");
+        setFont(textView3,"Roboto-Black.ttf");
+    }
+
+    public void setFont(TextView textView, String fontName) {
+        if(fontName != null){
+            try {
+                Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/" + fontName);
+                textView.setTypeface(typeface);
+            } catch (Exception e) {
+                Log.e("FONT", fontName + " not found", e);
+            }
+        }
+    }
+}
---
+++ CustomFont/v2/app/src/main/java/com/tutosandroidfrance/customfont/CustomTextView.java
@@ -0,0 +1,41 @@
+package com.tutosandroidfrance.customfont;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * Created by florentchampigny on 30/06/15.
+ */
+public class CustomTextView extends TextView {
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        //Typeface.createFromAsset doesn't work in the layout editor. Skipping...
+        if (isInEditMode()) {
+            return;
+        }
+
+        TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.CustomTextView);
+        String fontName = styledAttrs.getString(R.styleable.CustomTextView_font);
+        styledAttrs.recycle();
+
+        setTypeFace(fontName);
+    }
+
+    public void setTypeFace(String fontName) {
+        if(fontName != null){
+            try {
+                Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName);
+                setTypeface(typeface);
+            } catch (Exception e) {
+                Log.e("FONT", fontName + " not found", e);
+            }
+        }
+    }
+
+}
