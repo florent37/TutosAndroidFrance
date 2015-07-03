9ac968b88d135372cc9b18eef1b4a9320c6281d4
Florent <champigny.florent@gmail.com>
2015-07-03 22:13:50 +0200

    updated butterknife

--- ButterKnifeSample/app/src/main/java/tutosandroidfrance/butterknifesample/MainActivity.java
+++ ButterKnifeSample/app/src/main/java/tutosandroidfrance/butterknifesample/MainActivity.java
@@ -1,41 +1,36 @@
 package tutosandroidfrance.butterknifesample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
+import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @InjectView(R.id.view1)
+    @Bind(R.id.view1)
     TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.inject(this);
+        ButterKnife.bind(this);
 
         textView.setText("Hello !");
     }
 
     @OnClick(R.id.view1)
-    void clicVueHaute(){
-        Toast.makeText(this,"Hello World !",Toast.LENGTH_SHORT).show();
+    void clicVueHaute() {
+        Toast.makeText(this, "Hello World !", Toast.LENGTH_SHORT).show();
     }
 
-    @OnClick({R.id.view2,R.id.view3,R.id.view4})
-    void clicVue(TextView textView){
-        Toast.makeText(this,textView.getText().toString(),Toast.LENGTH_SHORT).show();
+    @OnClick({R.id.view2, R.id.view3, R.id.view4})
+    void clicVue(TextView textView) {
+        Toast.makeText(this, textView.getText().toString(), Toast.LENGTH_SHORT).show();
 
     }
 }
