909a57e1711601d582254a635096983a0f8abba5
Florent <champigny.florent@gmail.com>
2015-07-03 21:49:16 +0200

    added GoogleMapsSample

---
+++ GoogleMapsSample/app/src/main/java/com/tutosandroidfrance/googlemapssample/MainActivity.java
@@ -0,0 +1,40 @@
+package com.tutosandroidfrance.googlemapssample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final LatLng PARIS = new LatLng(48.858093, 2.294694);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+
+                googleMap.addMarker(new MarkerOptions().title("Paris").position(PARIS));
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(PARIS, 15));
+                googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+            }
+        });
+    }
+
+}
